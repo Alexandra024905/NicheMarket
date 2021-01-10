defd6ad7e45df50847c166747b2ceb4f39aea6b6
alexandra.a.2016 <alexandra.a.2016@feg.bg>
2021-01-10 16:59:56 +0200

    First commit

---
+++ Niche Market Data.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Niche_Market_Data.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ Niche Market Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Niche_Market_Data
+{
+    public class Class1
+    {
+    }
+}
