b7555e459d987d96cadb34beea0e74c174cffd60
alexandra.a.2016 <alexandra.a.2016@feg.bg>
2021-01-27 09:32:30 +0200

    Revert "First commit"
    This reverts commit defd6ad7e45df50847c166747b2ceb4f39aea6b6.

--- Niche Market Data.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Niche_Market_Data.Models
-{
-    public class Class1
-    {
-    }
-}
--- Niche Market Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Niche_Market_Data
-{
-    public class Class1
-    {
-    }
-}
